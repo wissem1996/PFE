54cc67a9a1764f66ef4de150244c29a9636a1c8e
wissem1996 <wghrissi5@gmail.com>
2023-10-26 17:12:12 +0100

    test2

--- angular-14-client/entrypoint.sh
+++ angular-14-client/entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-#echo ${BACKEND} | sed -i "s#backend:8080#${BACKEND}#g" /usr/share/nginx/html/*.*
+echo ${BACKEND} | sed -i "s#http://backend:8080/api/tutorials#${BACKEND}#g" /usr/share/nginx/html/*.*
+
 
 exec nginx -g "daemon off;"
