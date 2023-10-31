1e4035f3bfe5a02b4668e6f1a72a054b9de32df7
wissem1996 <54236599+wissem1996@users.noreply.github.com>
2023-10-31 01:05:22 +0100

    Update tutorial.service.ts

--- angular-14-client/src/app/services/tutorial.service.ts
+++ angular-14-client/src/app/services/tutorial.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Tutorial } from '../models/tutorial.model';
 
-const baseUrl = 'http://localhost:80/api/tutorials';
+const baseUrl = 'http://backend:8082/api/tutorials';
 
 @Injectable({
   providedIn: 'root'
